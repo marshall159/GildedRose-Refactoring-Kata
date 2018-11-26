a5bb1a14d9118ac73742647b668d05fd8a756769
Aneel Marshall <aneel.marshall@outlook.com>
2018-11-26 20:51:14 +0000

    refactor: add #backstage_after_concert to set quality to zero

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -10,20 +10,20 @@ class GildedRose
 
       decrease_sell_in(item)
 
-      backstage_passes_increase_quality_near_sellin(item) if backstage_passes?(item)
-
       if item.name == "Aged Brie" or backstage_passes?(item)
         increase_quality(item)
       else
         decrease_quality(item)
       end
 
+      backstage_passes_increase_quality_near_sellin(item) if backstage_passes?(item)
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             decrease_quality(item)
           else
-            item.quality = item.quality - item.quality # Backstage passes after concert drops to zero
+            backstage_after_concert(item)
           end
         else
           increase_quality(item)
@@ -66,6 +66,10 @@ class GildedRose
       if item.sell_in < 6
         increase_quality(item)
       end
+  end
+
+  def backstage_after_concert(item) 
+    item.quality = item.quality - item.quality
   end
 
 end
