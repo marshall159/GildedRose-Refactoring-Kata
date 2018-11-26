dbdc517dfbf67575a01b3b54cfe5745d96b73ff6
Aneel Marshall <aneel.marshall@outlook.com>
2018-11-26 13:11:32 +0000

    test: refactor tests using context blocks and let statements

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,30 +1,24 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
+  let(:items) { [Item.new("foo", 20, 20)] }
+  let(:gilded_rose) { described_class.new(items) }
 
   describe "#update_quality" do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
 
-      GildedRose.new(items).update_quality()
+      gilded_rose.update_quality()
 
       expect(items[0].name).to eq "foo"
     end
 
     it "decreases the #sell_in value" do
-      items = [Item.new("foo", 20, 20)]
-
-      gr = GildedRose.new(items)
-
-      expect { gr.update_quality() }.to change { items[0].sell_in }.by(-1)
+      expect { gilded_rose.update_quality() }.to change { items[0].sell_in }.by(-1)
     end
 
     it "decreases the #quality value" do
-      items = [Item.new("foo", 20, 20)]
-
-      gr = GildedRose.new(items)
-
-      expect { gr.update_quality() }.to change { items[0].quality }.by(-1)
+      expect { gilded_rose.update_quality() }.to change { items[0].quality }.by(-1)
     end
 
     it "#quality is never negative" do 
@@ -63,38 +57,41 @@ describe GildedRose do
       expect { gr.update_quality() }.to change { items[0].quality }.by(-2)
     end
 
-    it "Sulfuras #sell_in and #quality does not decrease" do
-      items = [Item.new("Sulfuras, Hand of Ragnaros", 20, 20)]
+    context "Sulfuras" do
+      it "#sell_in and #quality does not decrease" do
+        items = [Item.new("Sulfuras, Hand of Ragnaros", 20, 20)]
 
-      GildedRose.new(items)
+        GildedRose.new(items)
 
-      expect(items[0].quality).to eq(20)
-      expect(items[0].sell_in).to eq(20)
+        expect(items[0].quality).to eq(20)
+        expect(items[0].sell_in).to eq(20)
+      end
     end
 
-    it "Backstage passes #quality increases by 2 when #sell_in is 10 days or less" do
-      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 30)]
+    context "Backstage passses" do 
+      it "#quality increases by 2 when #sell_in is 10 days or less" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 30)]
 
-      gr = GildedRose.new(items)
+        gr = GildedRose.new(items)
 
-      expect { gr.update_quality() }.to change { items[0].quality }.by(2)
-    end
+        expect { gr.update_quality() }.to change { items[0].quality }.by(2)
+      end
 
-    it "Backstage passes #quality increases by 3 when #sell_in is 5 days or less" do
-      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 30)]
+      it "#quality increases by 3 when #sell_in is 5 days or less" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 30)]
 
-      gr = GildedRose.new(items)
+        gr = GildedRose.new(items)
 
-      expect { gr.update_quality() }.to change { items[0].quality }.by(3)
-    end
+        expect { gr.update_quality() }.to change { items[0].quality }.by(3)
+      end
 
-    it "Backstage passes #quality drops to 0 after concert when #sell_in is < 0" do
-      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 30)]
+      it "#quality drops to 0 after concert when #sell_in is < 0" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 30)]
 
-      GildedRose.new(items).update_quality()
+        GildedRose.new(items).update_quality()
 
-      expect(items[0].quality).to eq(0)
+        expect(items[0].quality).to eq(0)
+      end
     end
   end
-
 end
